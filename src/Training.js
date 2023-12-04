de53b679b6e5e144570e7afb1fb2c3bd3a383143
acvijic <aleksa.cvijic@student.um.si>
2023-12-04 12:02:36 +0100

    Training API connection

--- src/Training.js
+++ src/Training.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Training.css";
 
-const trainingPrograms = [
-  { id: 1, title: "Beginner Bootcamp", duration: "4 weeks" },
-  { id: 2, title: "Marathon Prep", duration: "12 weeks" },
-  { id: 3, title: "Strength Training", duration: "6 weeks" },
-];
-
 const Training = () => {
+  const [trainingPrograms, setTrainingPrograms] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/training")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data); // Log the data received from the API
+        setTrainingPrograms(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div className="training-container">
       <h2>Training Programs</h2>
       {trainingPrograms.map((program) => (
-        <div key={program.id} className="training-item">
-          <h3 className="training-title">{program.title}</h3>
+        <div key={program.trainingID} className="training-item">
+          <h3 className="training-title">{program.name}</h3>
           <p className="training-duration">Duration: {program.duration}</p>
+          <p className="training-description">
+            Description: {program.description}
+          </p>
+          {/* You can display other fields like participants and progress here */}
         </div>
       ))}
     </div>
